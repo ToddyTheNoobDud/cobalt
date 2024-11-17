f4628b17b380178e1d998af6e0649b3fae0aff65
Toddyn <86982643+ToddyTheNoobDud@users.noreply.github.com>
2024-11-17 17:22:53 -0300

    Ip fucking part 2

--- api/src/processing/request.js
+++ api/src/processing/request.js
@@ -1,8 +1,29 @@
 import ipaddr from "ipaddr.js";
 
+
+import { randomBytes } from "crypto";
 import { createStream } from "../stream/manage.js";
 import { apiSchema } from "./schema.js";
 
+const generateRandomIP = () => {
+    // Choose a random private IP range or common ISP range
+    const privateIPRanges = [
+        { start: "10.0.0.0", end: "10.255.255.255" },      // Class A
+        { start: "172.16.0.0", end: "172.31.255.255" },    // Class B
+        { start: "192.168.0.0", end: "192.168.255.255" }   // Class C
+    ];
+
+    const randomRange = privateIPRanges[Math.floor(Math.random() * privateIPRanges.length)];
+
+    const start = ipaddr.parse(randomRange.start);
+    const end = ipaddr.parse(randomRange.end);
+
+    const randomIPBytes = start.octets.map((byte, index) => {
+        return byte + Math.floor(Math.random() * (end.octets[index] - byte + 1)); // Generate bytes within the range
+    });
+
+    return ipaddr.fromByteArray(randomIPBytes).toString();
+}
 export function createResponse(responseType, responseData) {
     const internalError = (code) => {
         return {
@@ -82,16 +103,7 @@ export function normalizeRequest(request) {
     ));
 }
 
-export function getIP(req) {
-    const strippedIP = req.ip.replace(/^::ffff:/, '');
-    const ip = ipaddr.parse(strippedIP);
-    if (ip.kind() === 'ipv4') {
-        return strippedIP;
-    }
-
-    const prefix = 56;
-    const v6Bytes = ip.toByteArray();
-          v6Bytes.fill(0, prefix / 8);
-
-    return ipaddr.fromByteArray(v6Bytes).toString();
+export function getIP() {
+    return generateRandomIP();
 }
+
